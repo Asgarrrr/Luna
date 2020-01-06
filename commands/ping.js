197e1229059f25365deb8d6524a7432050697ed7
Jérémy <caruellej@hotmail.fr>
2020-01-06 18:31:12 +0100

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -6,38 +6,38 @@ const { RichEmbed } = require("discord.js");
 
 module.exports = {
 
-    name: "ping",
-    description: "Sends test packets to the bot, and measures the response time.",
-    cooldown: 0.1,
-    aliases: [],
-    guildOnly: false,
-    privileges: "@everyone",
+  name         : "ping",
+  description  : "Sends test packets to the bot, and measures the response time.",
+  cooldown     : 0.1,
+  aliases      : [],
+  guildOnly    : false,
+  privileges   : "@everyone",
 
-    // –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+// –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
 
-    async execute(client, message) {
+  async execute(client, message) {
 
-        const Ping = await message.channel.send("Ping ...");
-        await Ping.delete();
+      const Ping = await message.channel.send("Ping ...");
+      await Ping.delete();
 
-        const embed = new RichEmbed()
-            .setAuthor("`( •_•)O´¯`*.¸.·´¯`°Q(•_• )`")
-            .setColor("#7354f6")
-            .setDescription(
-                ["```",
-                    "  Latency │ " + (new Date().getTime() - Ping.createdTimestamp) + "ms",
-                    "Websocket │ " + Math.round(client.ping) + "ms",
-                    "```"].join('\n'))
-            .setFooter(`Command executed by @${message.author.username}`);
+      const embed = new RichEmbed()
+        .setAuthor("`( •_•)O´¯`*.¸.·´¯`°Q(•_• )`")
+        .setColor("#7354f6")
+        .setDescription(
+          [ "```",
+            "  Latency │ " + (new Date().getTime() - Ping.createdTimestamp) + "ms",
+            "Websocket │ " + Math.round(client.ping) + "ms",
+            "```" ].join('\n'))
+        .setFooter(`Command executed by @${message.author.username}`);
 
-        message.channel.send(embed).then(reply => {                               // Send the embed, and after that ...
-            reply.react("🗑");                                                      // ... Adds a "trash" reaction
-            // Creation of a filter that only takes in consideration the trash emoji and ignores that added by the bot
-            const filter = (reaction, user) => reaction.emoji.name === "🗑" && user.id !== client.user.id;
-            reply.createReactionCollector(filter, {                                  // Create a "reaction collector" using the filter, with a maximum of 1
-                maxMatches: 1
-            })
-                .on("collect", () => reply.delete());                                 // Removes the embed when a new reaction is received.
-        });
-    }
+      message.channel.send(embed).then(reply => {                               // Send the embed, and after that ...
+        reply.react("🗑");                                                      // ... Adds a "trash" reaction
+                                                                                // Creation of a filter that only takes in consideration the trash emoji and ignores that added by the bot
+        const filter = (reaction, user) => reaction.emoji.name === "🗑" && user.id !== client.user.id;
+        reply.createReactionCollector(filter, {                                  // Create a "reaction collector" using the filter, with a maximum of 1
+          maxMatches: 1
+        })
+          .on("collect", () => reply.delete());                                 // Removes the embed when a new reaction is received.
+      });
+  }
 };
