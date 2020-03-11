e01934b5aacee5c9135784d66c2705f793b20e51
Jérémy <caruellej@hotmail.fr>
2020-03-11 10:42:22 +0100

    Update avatar.js

--- commands/General/avatar.js
+++ commands/General/avatar.js
@@ -7,14 +7,14 @@ const { MessageEmbed } = require("discord.js");
 
 module.exports = {
 
-    name            : "avatar",
-    description     : "Returns the profile picture of a mentioned user.",
-    cooldown        : 3,
-    aliases         : ["pp"],
-    guildOnly       : true,
-    privileges      : ["SEND_MESSAGES"],
+    name: "avatar",
+    description: "Returns the profile picture of a mentioned user.",
+    cooldown: 3,
+    aliases: ["pp"],
+    guildOnly: true,
+    privileges: ["SEND_MESSAGES"],
 
-// –––––– Execution ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+    // –––––– Execution ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
 
     async execute(Glossary, client, message, args) {
 
@@ -22,19 +22,19 @@ module.exports = {
         await message.delete();
 
         // Shortens the link to the user if it is pinged
-        const IsPing  = message.mentions.members.first(),
-        // Retrieves the user's information if it is pinged, or search the username; otherwise use the applicant's information.
-              ReqUser = (IsPing ? IsPing.user : client.users.cache.find((x) => x.username === args[0]));
+        const IsPing = message.mentions.members.first(),
+            // Retrieves the user's information if it is pinged, or search the username; otherwise use the applicant's information.
+            ReqUser = (IsPing ? IsPing.user : client.users.cache.find((x) => x.username === args[0]) || message.member.user);
         // Generation of an embed to format and send the transmitted information
         message.channel.send(
             new MessageEmbed()
-                .setColor("#36393f")
-                .setAuthor(ReqUser === message.author ? `${Glossary.COM_Avatar[1]} ${ReqUser.username}` : `${Glossary.COM_Avatar[2]} ${ReqUser.username}, ${message.member.user.username}`, message.member.user.avatarURL())
-                .setImage(ReqUser.avatarURL({
-                    format  : 'png',
-                    dynamic : true,
-                    size    : 1024
-                }))
+            .setColor("#36393f")
+            .setAuthor(ReqUser === message.author ? `${Glossary.COM_Avatar[1]} ${ReqUser.username}` : `${Glossary.COM_Avatar[2]} ${ReqUser.username}, ${message.member.user.username}`, message.member.user.avatarURL())
+            .setImage(ReqUser.avatarURL({
+                format: 'png',
+                dynamic: true,
+                size: 1024
+            }))
         ).then((reply) => {
             // ... Adds a "trash" reaction
             reply.react("🗑");
@@ -42,10 +42,10 @@ module.exports = {
             const filter = (reaction, user) => reaction.emoji.name === "🗑" && user.id !== client.user.id;
             // Create a "reaction collector" using the filter, with a maximum of 1
             reply.createReactionCollector(filter, {
-                maxMatches: 1
-            })
-            // Removes the embed when a new reaction is received.
-            .on("collect", () => reply.delete());
+                    maxMatches: 1
+                })
+                // Removes the embed when a new reaction is received.
+                .on("collect", () => reply.delete());
         });
     }
 };
