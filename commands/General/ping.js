043fdaf98e1ddb1edfbbb728103fc2ef78a1eed2
Jérémy <caruellej@hotmail.fr>
2020-03-13 14:21:52 +0100

    Update ping.js

--- commands/General/ping.js
+++ commands/General/ping.js
@@ -2,6 +2,8 @@
 
 // A powerful library for interacting with the Discord API
 const { MessageEmbed } = require("discord.js"),
+// Import custom function (avoid duplicated block)
+      { DelAfterSend } = require("../../resources/Functions.js"),
 // The simplified HTTP request client 'request' with Promise support. Powered by Bluebird.
       rp               = require("request-promise");
 
@@ -21,77 +23,70 @@ module.exports = {
     async execute(Glossary, client, message) {
 
         const Lat = new Date().getTime() - message.createdTimestamp;
-        var PResu = new MessageEmbed();
+        var embed = new MessageEmbed();
 
         // Retrieve status information
         await rp("https://srhpyqt94yxb.statuspage.io/api/v2/summary.json", { json: true })
             .then((DiscordappStatus) => {
 
-                PResu
-                .setTitle(" Ｐ Ｏ Ｎ Ｇ !")
-                .setColor("#7354f6")
-                .addFields(
-                    { name  : "— ヽ( •_•)O´¯\\`°.¸.·´¯\\`Q(^o^ )\\`",
-                        value : [
-                        "```",
-                        `   ${Glossary.COM_Ping[0]} ${Lat}ms`,
-                        ` ${Glossary.COM_Ping[1]} ${Math.round(client.ws.ping)}ms`,
+                // Load Glossary
+                Glossary = Glossary.get("commands").ping(Lat, client, message);
 
-                        `CloudFlare │ ${DiscordappStatus.components[4].status ? "OK !" : "✗"}`,
-                        `       API │ ${DiscordappStatus.components[0].status ? "OK !" : "✗"}`,
-                        `   Gateway │ ${DiscordappStatus.components[3].status ? "OK !" : "✗"}`,
-                        `Med. Proxy │ ${DiscordappStatus.components[6].status ? "OK !" : "✗"}`,
-                        `     Voice │ ${DiscordappStatus.components[7].status ? "OK !" : "✗"}`,
-                        "```"
-                        ].join("\n")
-                    },
-                    { name  : "— Servers Status",
-                        value : ["```",
-                        `   EU West │ ${DiscordappStatus.components[1].status ? "✔" : "✗"} : ${DiscordappStatus.components[12].status ? "✔" : "✗"} │ US West`,
-                        `EU Central │ ${DiscordappStatus.components[2].status ? "✔" : "✗"} : ${DiscordappStatus.components[13].status ? "✔" : "✗"} │ Brazil`,
-                        ` Singapore │ ${DiscordappStatus.components[5].status ? "✔" : "✗"} : ${DiscordappStatus.components[14].status ? "✔" : "✗"} │ Hong Kong`,
-                        `    Sydney │ ${DiscordappStatus.components[8].status ? "✔" : "✗"} : ${DiscordappStatus.components[15].status ? "✔" : "✗"} │ Russia`,
-                        `US Central │ ${DiscordappStatus.components[9].status ? "✔" : "✗"} : ${DiscordappStatus.components[16].status ? "✔" : "✗"} │ Japan`,
-                        `   US East │ ${DiscordappStatus.components[10].status ? "✔" : "✗"} : ${DiscordappStatus.components[17].status ? "✔" : "✗"} │ South Afr`,
-                        `  US South │ ${DiscordappStatus.components[11].status ? "✔" : "✗"} :   │ `,
-                        "```"
-                        ].join("\n")
-                    },
-                    { name  : "— Maintenance & Incidents",
-                        value : ["```",
-                        `     Event │ ${DiscordappStatus.incidents}${DiscordappStatus.scheduled_maintenances}`,
-                        "```",
-                        "[Discordapp Status](https://status.discordapp.com/)"
-                        ].join("\n")
-                    })
-
-                .setFooter(`${Glossary.COM_Ping[2]}${message.author.username}`)
-                .setTimestamp();
-
+                embed
+                    .setTitle(" Ｐ Ｏ Ｎ Ｇ !")
+                    .setColor("#7354f6")
+                    .addFields(
+                        {   name  : "— ヽ( •_•)O´¯\\`°.¸.·´¯\\`Q(^o^ )\\`",
+                            value : [
+                                "```",
+                                Glossary[0],
+                                Glossary[1],
+                                `CloudFlare │ ${DiscordappStatus.components[4].status ? "OK !" : "✗"}`,
+                                `       API │ ${DiscordappStatus.components[0].status ? "OK !" : "✗"}`,
+                                `   Gateway │ ${DiscordappStatus.components[3].status ? "OK !" : "✗"}`,
+                                `Med. Proxy │ ${DiscordappStatus.components[6].status ? "OK !" : "✗"}`,
+                                `     Voice │ ${DiscordappStatus.components[7].status ? "OK !" : "✗"}`,
+                                "```"
+                            ].join("\n")
+                        },
+                        {   name  : Glossary[2],
+                            value : [
+                                "```",
+                                `   EU West │ ${DiscordappStatus.components[1].status ? "✔" : "✗"} : ${DiscordappStatus.components[12].status ? "✔" : "✗"} │ US West`,
+                                `EU Central │ ${DiscordappStatus.components[2].status ? "✔" : "✗"} : ${DiscordappStatus.components[13].status ? "✔" : "✗"} │ Brazil`,
+                                ` Singapore │ ${DiscordappStatus.components[5].status ? "✔" : "✗"} : ${DiscordappStatus.components[14].status ? "✔" : "✗"} │ Hong Kong`,
+                                `    Sydney │ ${DiscordappStatus.components[8].status ? "✔" : "✗"} : ${DiscordappStatus.components[15].status ? "✔" : "✗"} │ Russia`,
+                                `US Central │ ${DiscordappStatus.components[9].status ? "✔" : "✗"} : ${DiscordappStatus.components[16].status ? "✔" : "✗"} │ Japan`,
+                                `   US East │ ${DiscordappStatus.components[10].status ? "✔" : "✗"} : ${DiscordappStatus.components[17].status ? "✔" : "✗"} │ South Afr`,
+                                `  US South │ ${DiscordappStatus.components[11].status ? "✔" : "✗"} :   │ `,
+                                "```"
+                            ].join("\n")
+                        },
+                        {   name  : "— Maintenance & Incidents",
+                            value : [
+                                "```",
+                                `     Event │ ${DiscordappStatus.incidents}${DiscordappStatus.scheduled_maintenances}`,
+                                "```",
+                                "[Discordapp Status](https://status.discordapp.com/)"
+                            ].join("\n")
+                        })
+                        .setFooter(Glossary[3])
+                        .setTimestamp();
             })
             .catch(() => {
 
-                PResu
-                .setDescription([
-                    "```",
-                    `   ${Glossary.COM_Ping[0]} ${Lat}ms`,
-                    ` ${Glossary.COM_Ping[1]} ${Math.round(client.ws.ping)}ms`,
-                    "```",
-                ].join("\n"));
+                embed
+                    .setDescription([
+                        "```",
+                        Glossary[0],
+                        Glossary[1],
+                        "```",
+                    ].join("\n"))
+                    .setFooter(Glossary[3])
+                    .setTimestamp();
             });
 
-        // Send the embed, and after that ...
-        await message.channel.send(PResu).then((reply) => {
-            // ... Adds a "trash" reaction
-            reply.react("🗑");
-            // Creation of a filter that only takes in consideration the trash emoji and ignores that added by the bot
-            const filter = (reaction, user) => reaction.emoji.name === "🗑" && user.id !== client.user.id;
-            // Create a "reaction collector" using the filter, with a maximum of 1
-            reply.createReactionCollector(filter, {
-                maxMatches: 1
-            })
-            // Removes the embed when a new reaction is received.
-            .on("collect", () => reply.delete());
-        });
+        // Send the embed and add a reaction to be able to remove it.
+        DelAfterSend(client, message, embed)
     }
 };
