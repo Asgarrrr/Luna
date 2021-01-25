fa953e6347d004b127ce22cc14af1c4032a847af
Jérémy Caruelle <36574651+Asgarrrrr@users.noreply.github.com>
2021-01-25 01:50:02 +0100

    FIX: 'port' is not defined & 'normalizePort' 'onError' and 'onListening' was used before it was defined.

--- src/Dashboard/app.js
+++ src/Dashboard/app.js
@@ -20,6 +20,56 @@ const indexRouter = require("./routes/index")
 
 module.exports = (client) => {
 
+    // —— Normalize a port into a number, string, or false.
+    function normalizePort(val) {
+        const port = parseInt(val, 10);
+
+        if (isNaN(port))
+            return val;
+
+        if (port >= 0)
+            return port;
+
+        return false;
+    }
+
+
+    const port = normalizePort(client.config.dashboard.port || "3000");
+
+    // —— Event listener for HTTP server "error" event.
+    function onError(error) {
+        if (error.syscall !== "listen") {
+            throw error;
+        }
+
+        const bind = typeof port === "string"
+            ? "Pipe " + port
+            : "Port " + port;
+
+        // —— Handle specific listen errors with friendly messages
+        switch (error.code) {
+            case "EACCES":
+                console.error(bind + " requires elevated privileges");
+                process.exit(1);
+                break;
+            case "EADDRINUSE":
+                console.error(bind + " is already in use");
+                process.exit(1);
+                break;
+            default:
+                throw error;
+        }
+    }
+
+    // —— Event listener for HTTP server "listening" event.
+    function onListening() {
+        const addr = server.address();
+        const bind = typeof addr === "string"
+            ? "pipe " + addr
+            : "port " + addr.port;
+        console.log("Listening on " + bind);
+    }
+
     const app = express();
 
     app
@@ -47,7 +97,7 @@ module.exports = (client) => {
 
         .set("view engine", "ejs")
         .set("views", path.join(__dirname, "/views"))
-        .set("port", normalizePort(client.config.dashboard.port || "3000"));
+        .set("port", port);
 
         const server = http.createServer(app);
 
@@ -71,49 +121,4 @@ module.exports = (client) => {
         res.render("error");
     });
 
-    function normalizePort(val) {
-        const port = parseInt(val, 10);
-
-        if (isNaN(port))
-          return val;
-
-        if (port >= 0)
-          return port;
-
-        return false;
-    }
-
-    // —— Event listener for HTTP server "error" event.
-    function onError(error) {
-        if (error.syscall !== "listen") {
-            throw error;
-        }
-
-        const bind = typeof port === "string"
-          ? "Pipe " + port
-          : "Port " + port;
-
-        // —— Handle specific listen errors with friendly messages
-        switch (error.code) {
-          case "EACCES":
-            console.error(bind + " requires elevated privileges");
-            process.exit(1);
-            break;
-          case "EADDRINUSE":
-            console.error(bind + " is already in use");
-            process.exit(1);
-            break;
-          default:
-            throw error;
-        }
-      }
-
-    // —— Event listener for HTTP server "listening" event.
-    function onListening() {
-        const addr = server.address();
-        const bind = typeof addr === "string"
-            ? "pipe " + addr
-            : "port " + addr.port;
-        console.log("Listening on " + bind);
-    }
 };
