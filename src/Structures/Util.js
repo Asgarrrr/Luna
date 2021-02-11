75d6c3e7594e0b04786fa429afe1ea5fe3f1668a
Jérémy Caruelle <36574651+Asgarrrrr@users.noreply.github.com>
2021-02-11 23:35:45 +0100

    Fix : Unexpected use of undefined.

--- src/Structures/Util.js
+++ src/Structures/Util.js
@@ -7,13 +7,13 @@ const chalk = require("chalk")
 
 // ██████ | ███████████████████████████████████████████████████████████ | ██████
 
-function resolveMention(query, guild = undefined) {
+function resolveMention(query, guild) {
 
     // —— Throwing any necessary errors.
     if (typeof query !== "string")
         throw new TypeError("Invalid string provided.");
 
-    if (typeof guild !== "undefined" && !(guild instanceof Discord.Guild))
+    if (guild && !(guild instanceof Discord.Guild))
         throw new TypeError("Invalid guild provided.");
 
     // —— Using a Regular Expression to test the mention and extract the parts.
@@ -38,11 +38,11 @@ function resolveMention(query, guild = undefined) {
 }
 
 
-    /**
-     * Search in all guilds or only one specific user by his ID or username.
-     * @param {string}  query  // User mention
-     * @param {object}  [guild] // query only on specific guild
-     */
+/**
+ * Search in all guilds or only one specific user by his ID or username.
+ * @param {string}  query  // User mention
+ * @param {object}  [guild] // query only on specific guild
+ */
 async function resolveUser(query, guild = undefined) {
 
         // —— Throwing any necessary errors.
@@ -57,82 +57,82 @@ async function resolveUser(query, guild = undefined) {
         if (match && guild)
             return guild.members.cache.get(match[1]);
 }
-    /**
-    * Search in all guilds or only one specific channel by his ID or name.
-    * @param {string}  query  // User ID or Username
-    * @param {object}  [guild] // Search only on specific guild
-    */
-    async function resolveChannel(query, guild) {
+/**
+* Search in all guilds or only one specific channel by his ID or name.
+* @param {string}  query  // User ID or Username
+* @param {object}  [guild] // Search only on specific guild
+*/
+async function resolveChannel(query, guild) {
 
-        if (!query) return;
+    if (!query) return;
 
-        return !isNaN(query) || query.match(/^<#(!|&)?(\d+)>$/)
-            ? (
-                query = query.replace(/\D/g, ""),
-                guild
-                    ? guild.channels.cache.get(query)
-                    : await this.channels.fetch(query).catch(() => 0)
-            )
-            : (guild ? guild.channels : this.channels).cache.find((x) => x.name === query);
+    return !isNaN(query) || query.match(/^<#(!|&)?(\d+)>$/)
+        ? (
+            query = query.replace(/\D/g, ""),
+            guild
+                ? guild.channels.cache.get(query)
+                : await this.channels.fetch(query).catch(() => 0)
+        )
+        : (guild ? guild.channels : this.channels).cache.find((x) => x.name === query);
 
+}
+
+/**
+* Adds a user to the database
+* @param {object}  user     // Guild user object
+* @param {object}  guild    // Guild object
+*/
+async function createUser(user, guild) {
+
+    user = await this.resolveUser(user.id, guild);
+
+    try {
+        this.db
+            .prepare("INSERT OR REPLACE INTO Members (_ID, UserID, GuildID, Guildname, Roles, JoinDate) VALUES (?, ?, ?, ?, ?, ?)")
+            .run(`${guild.id}-${user.id}`, user.id, guild.id, guild.name, JSON.stringify(user._roles), user.joinedTimestamp);
+
+    } catch (error) {
+        console.error(error);
     }
 
-    /**
-    * Adds a user to the database
-    * @param {object}  user     // Guild user object
-    * @param {object}  guild    // Guild object
-    */
-    async function createUser(user, guild) {
+    return user;
 
-        user = await this.resolveUser(user.id, guild);
+}
 
-        try {
+async function logger(type = "INFO", message) {
+
+    if (this.config.logger !== true)
+        return;
+
+    const time = `${chalk.grey(new Date().toLocaleTimeString())}`;
+
+    switch (type) {
+        case "INFO": { console.log(`${time} │ ${message}`); }
+            break;
+
+        case "WARNING": {
+            console.log(`${time} ⨯ ${chalk.hex("#ba8b00")(message)}`);
             this.db
-                .prepare("INSERT OR REPLACE INTO Members (_ID, UserID, GuildID, Guildname, Roles, JoinDate) VALUES (?, ?, ?, ?, ?, ?)")
-                .run(`${guild.id}-${user.id}`, user.id, guild.id, guild.name, JSON.stringify(user._roles), user.joinedTimestamp);
-
-        } catch (error) {
-            console.error(error);
-        }
-
-        return user;
-
+                .prepare("INSERT INTO Event ('Type', 'DATA') VALUES ('WARNING', ?)")
+                .run(message);
+        } break;
     }
+}
 
-    async function logger(type = "INFO", message) {
-
-        if (this.config.logger !== true)
-            return;
-
-        const time = `${chalk.grey(new Date().toLocaleTimeString())}`;
-
-        switch (type) {
-            case "INFO": { console.log(`${time} │ ${message}`); }
-                break;
-
-            case "WARNING": {
-                console.log(`${time} ⨯ ${chalk.hex("#ba8b00")(message)}`);
-                this.db
-                    .prepare("INSERT INTO Event ('Type', 'DATA') VALUES ('WARNING', ?)")
-                    .run(message);
-            } break;
-        }
-    }
-
-    /**
-     * Converts a certain number of seconds to formatted time hh:mm:ss
-     * @param {number} seconds // Name of second to convert
-     */
-    function formatTime(seconds) {
-        const h = Math.floor(seconds / 3600);
-        const m = Math.floor((seconds % 3600) / 60);
-        const s = Math.round(seconds % 60);
-        return [
-          h,
-          m > 9 ? m : (h ? "0" + m : m || "0"),
-          s > 9 ? s : "0" + s,
-        ].filter(Boolean).join(":");
-    }
+/**
+ * Converts a certain number of seconds to formatted time hh:mm:ss
+ * @param {number} seconds // Name of second to convert
+ */
+function formatTime(seconds) {
+    const h = Math.floor(seconds / 3600);
+    const m = Math.floor((seconds % 3600) / 60);
+    const s = Math.round(seconds % 60);
+    return [
+        h,
+        m > 9 ? m : (h ? "0" + m : m || "0"),
+        s > 9 ? s : "0" + s,
+    ].filter(Boolean).join(":");
+}
 
 module.exports = {
     resolveMention,
