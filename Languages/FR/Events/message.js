7b14a74d8ea8e3be0d1f3068a2bc05f7f552dfd7
Jérémy Caruelle <36574651+Asgarrrrr@users.noreply.github.com>
2021-03-17 19:30:39 +0100

    Missing semicolon

--- Languages/FR/Events/message.js
+++ Languages/FR/Events/message.js
@@ -5,7 +5,7 @@ module.exports = {
     nsfw        : "Désolé, cette commande ne peut être exécutée que dans un salon NSFW..",
     cooldown    : ( command, message ) => `Veuillez attendre ${( ( command.cmdCooldown.get( message.author.id ) - Date.now() ) / 1000 ).toFixed(1) } seconde(s) pour réutiliser la commande ${command.name}.`,
     args        : ( message ) => `Vous n'avez fourni aucun argument, ${message.author} !`,
-    helpEmbed   : ( cmd ) => { return {
+    helpEmbed   : ( cmd, client ) => { return {
         title       : `${cmd.name.replace(/\b\w/g, (l) => l.toUpperCase())}`,
         description : `> *${cmd.description}*`,
         fields      : [{
@@ -15,8 +15,8 @@ module.exports = {
             name    : "Exemples",
             value   :`\`\`\`${cmd.exemple && cmd.exemple.map((x) => `${client.config.prefix}${cmd.name} ${x}`).join("\n") || "No examples provided"}\`\`\``,
         }],
-    }},
+    }; },
     "missPerm"  : "Je ne dispose pas des droits suffisants pour exécuter cette commande.",
     "youMiss"   : "Vous n'avez pas les privilèges nécessaires pour exécuter cette commande..."
 
-}
+};
