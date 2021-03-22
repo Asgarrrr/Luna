35800361e71804add645a337d3955d0747dd6aa1
Jérémy Caruelle <36574651+Asgarrrrr@users.noreply.github.com>
2021-03-22 20:22:45 +0100

    Add translation

--- Languages/FR/Administration/autorole.js
+++ Languages/FR/Administration/autorole.js
@@ -4,10 +4,12 @@ module.exports = {
     notEnabled      : "L'attribution automatique est cependant désactivée",
     currentRoles    : ( currentRoles ) => `Les roles ${ currentRoles.map( ( x ) => `<@&${x}>` ).join( ", ") } seront automatiquement attribués aux nouveaux arrivants.`,
     disabled        : "L'attribution automatique des rôles a été désactivée !",
-    noRole          : "Aucun rôle à ajouter",
+    cantAdd         : ( roles ) => `Les roles ${ roles.join( ", " ) } ne peuvent pas être ajoutés. Leurs privilèges sont au dessus des miens, je ne peux pas les donner`,
+    noRole          : "Vous devez spécifier un ou plusieurs rôles à ajouter / supprimer",
     noChanges       : ( operation ) => `Aucun changement, l'attribution automatique était déjà ${ operation ? "enabled" : "disabled" } `,
+    nothingToAdd    : "Il n'y a aucun rôle à ajouter",
     error           : "Impossible de faire cela, une erreur s'est produite ...",
-    missPerms       : "Les rôles suivants ne peuvent pas être ajoutés (permissions)",
-    assigned        : "Les rôles définis sont les suivants",
+    missPerms       : "Les rôles suivants ne peuvent pas être ajoutés",
+    assigned        : "Les rôles définis sont",
 
 };
