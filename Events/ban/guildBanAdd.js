3903ffa4b1812055b3af1923d7b67886d70b6eb8
Jérémy Caruelle <36574651+Asgarrrrr@users.noreply.github.com>
2021-03-27 03:28:59 +0100

    Adding the log message

--- Events/ban/guildBanAdd.js
+++ Events/ban/guildBanAdd.js
@@ -5,6 +5,7 @@ const Event = require( "../../Structures/Event" );
 
 // ██████ | █████████████████████████████████████████████████████████████████████
 
+// —— Create & export a class for the event that extends the base event
 class guildBanAdd extends Event {
 
     constructor( client ) {
@@ -13,28 +14,42 @@ class guildBanAdd extends Event {
 
     async run( guild, user ) {
 
+        const { guildBanAdd: language } = this.client.language[ guild.local ];
+
         try {
 
             const data = await guild.fetchAuditLogs({
                 limit   : 1,
                 type    : "MEMBER_BAN_ADD",
-                user
             });
 
-            if (!data.entries)
-                return "Mhhhh";
+            // —— Selects the first result
+            const banned = data.entries.first();
 
-            console.log(data.entries)
+            // —— Sending a confirmation message if this option is activated
+            if ( guild.plugins.logger.enabled ) {
+
+                // —— Get the chosen channel
+                const channel = guild.channels.cache.get( guild.plugins.logger.channel );
+
+                // —— Sending a confirmation message
+                channel && channel.send( { embed : {
+                    title       : "Utilisateur banni",
+                    color       : "#f04747",
+                    timestamp   : Date.now(),
+                    // —— Let's do a consistency check here and make sure we got something.
+                    description : banned ? language.details( banned ) : language.missDetails( user )
+                }});
+
+            }
 
         } catch (error) {
-            console.log(error);
+
+            console.error(error);
+
         }
 
-
-
-
     }
-
 
 }
 
