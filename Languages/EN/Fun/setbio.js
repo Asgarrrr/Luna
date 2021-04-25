9a2c8cb239217c898d196523ba8795114f6e155b
Jérémy Caruelle <36574651+Asgarrrrr@users.noreply.github.com>
2021-04-25 08:55:07 +0200

    Allows you to define the background of the rank map

---
+++ Languages/EN/Fun/setbio.js
@@ -0,0 +1,7 @@
+module.exports = {
+
+    unchanged: "Your profile sentence hasn't been changed",
+    ok       : "Your profile sentence has been changed",
+    error    : "An error has occurred...",
+
+};
