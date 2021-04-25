3f11dd0b3839db2b0d4f7d1b5885dd7e850dafa8
Jérémy Caruelle <36574651+Asgarrrrr@users.noreply.github.com>
2021-04-25 08:55:41 +0200

    Revert "Allows you to define the background of the rank map"
    This reverts commit 9a2c8cb239217c898d196523ba8795114f6e155b.

--- Languages/EN/Fun/setbio.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-
-    unchanged: "Your profile sentence hasn't been changed",
-    ok       : "Your profile sentence has been changed",
-    error    : "An error has occurred...",
-
-};
