ba08721972f53ff330fa48c7bd636ec4613742c5
Jérémy Caruelle <36574651+Asgarrrrr@users.noreply.github.com>
2021-04-25 08:57:59 +0200

    Adding custom text to rank cards

--- Models/Member.js
+++ Models/Member.js
@@ -1,6 +1,6 @@
 const { model, Schema } = require("mongoose");
 
-module.exports = model("Member", new Schema({
+module.exports = model( "Member", new Schema({
 
     // —— Basic information
     _ID         : String,
@@ -10,6 +10,7 @@ module.exports = model("Member", new Schema({
     // —— Levelling system
     experience  : { type: Number, default: 0 },
     level       : { type: Number, default: 1 },
+    bio         : String,
 
     // —— Moderation register
 	mute        : {
