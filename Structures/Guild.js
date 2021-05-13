4aed8cd9993f072541cfb8fdfcf39bef2438c900
Jérémy Caruelle <36574651+Asgarrrrr@users.noreply.github.com>
2021-05-13 04:34:03 +0200

    Added a parameter to contain the player embed + a reset method

--- Structures/Guild.js
+++ Structures/Guild.js
@@ -18,9 +18,22 @@ Structures.extend( "Guild", ( Guild ) => class extends Guild {
             _dispatcher  : null,
             _isPlaying   : false,
             _volume      : 1,
-            _embed       : {},
+            _embedMsg    : null,
+            _embed       : null,
             _loop        : false,
             _ttl         : [0, 0],
+            reset        : () => {
+                this.player._queue      = [];
+                this.player._oldQueue   = [];
+                this.player._connection = null;
+                this.player._dispatcher = null;
+                this.player._isPlaying  = false;
+                this.player._volume     = 1;
+                this.player._embedMsg   = null;
+                this.player._embed      = null;
+                this.player._loop       = false;
+                this.player._ttl        = [0, 0];
+            }
         };
 
         this.client.db.Guild.findOneAndUpdate({
