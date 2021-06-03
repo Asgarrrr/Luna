4f9a030090eb622c528b6a3c285612d7fabf5caa
asgarrrr <caruellej@hotmail.fr>
2021-06-03 20:47:03 +0200

    You must BE a channel?

--- Languages/EN/Music/stop.js
+++ Languages/EN/Music/stop.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-    notInVoice  : "You must be a voice channel",
+    notInVoice  : "You must be in a voice channel",
     noConnected : "There is nothing playing",
     busy        : "Luna is already busy with other listeners, join them",
 
